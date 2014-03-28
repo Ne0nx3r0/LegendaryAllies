53ac21bddb6a8287dd74e175b5e44a5bd79b80a3
Ne0nx3r0 <Ne0nx3r0@viasmooth>
2014-03-28 08:52:09 -0500

    Removing redundant import

--- com/ne0nx3r0/legendaryallies/listeners/LegendaryAlliesEntityListener.java
+++ com/ne0nx3r0/legendaryallies/listeners/LegendaryAlliesEntityListener.java
@@ -2,7 +2,6 @@ package com.ne0nx3r0.legendaryallies.listeners;
 
 import com.earth2me.essentials.User;
 import com.ne0nx3r0.legendaryallies.LegendaryAlliesPlugin;
-import java.util.logging.Level;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
